2d50ed333d9e3467445dbaad98c5d1dcbce23d9b
Tom Schneider <ric0.github@gmail.com>
2022-02-08 00:12:12 +0100

    refactor: input-button-row component
    - replace array indexes with lodash.range()
    - use programmatic mapping to generate the button text, instead of html-templating
    - reduce duplicate code

--- src/app/input/input-button-row/input-button-row.component.ts
+++ src/app/input/input-button-row/input-button-row.component.ts
@@ -1,5 +1,11 @@
 import {Component} from '@angular/core';
 import {DartCounterService} from 'src/app/services/dart-counter.service';
+import {range} from 'lodash';
+
+interface DisplayNumber {
+  text: string,
+  value: number,
+}
 
 @Component({
   selector: 'app-input-button-row',
@@ -8,33 +14,51 @@ import {DartCounterService} from 'src/app/services/dart-counter.service';
 })
 export class InputButtonRowComponent {
 
-  arrayOf10 = Array(10);
+  singles: DisplayNumber[] = range(1, 21).map(this.mapToDisplayNumber);
+  doubles: DisplayNumber[] = this.singles.map(this.prefixDisplayNameBy('D')).map(this.multiplyValueBy(2));
+  triples: DisplayNumber[] = this.singles.map(this.prefixDisplayNameBy('T')).map(this.multiplyValueBy(3));
+  bull: DisplayNumber = {text: 'Bull', value: 25};
+  bullsEye: DisplayNumber = {text: `Bull's Eye`, value: 25};
+  miss: DisplayNumber = {text: 'Miss', value: 0};
 
   constructor(private dartCounterService: DartCounterService) {
   }
 
-  public reduceSingle(points: number) {
+  scorePoints(points: number) {
     this.dartCounterService.reduceCountBy(points);
   }
 
-  public reduceDouble(points: number) {
-    this.dartCounterService.reduceCountBy(points * 2);
-  }
-
-  public reduceTriple(points: number) {
-    this.dartCounterService.reduceCountBy(points * 3);
-  }
-
-  public reduceBull(points: number) {
-    this.dartCounterService.reduceCountBy(points);
-  }
-
-  public reduceBullsEye(points: number) {
-    this.dartCounterService.reduceCountBy(points);
-  }
-
-  public reduceDartCounter() {
+  reduceDartCounter() {
     this.dartCounterService.reduceDartCount();
+  }
+
+  // noinspection JSMethodCanBeStatic
+  /**
+   * Convert a primitive number into a {@link DisplayNumber}
+   * @param num
+   */
+  private mapToDisplayNumber(num: number): DisplayNumber {
+    return {text: num + '', value: num}
+  }
+
+  /**
+   * Curried function to multiply the value of a given {@link DisplayNumber}
+   *
+   * @param multiplier
+   * @returns a transformer function
+   */
+  private multiplyValueBy(multiplier: number): (input: DisplayNumber) => DisplayNumber {
+    return ({text, value}: DisplayNumber) => ({text, value: value * multiplier});
+  }
+
+  /**
+   * Curried function to add a prefix to the displayName of a given {@link DisplayNumber}
+   *
+   * @param prefix
+   * @returns a transformer function
+   */
+  private prefixDisplayNameBy(prefix: string): (input: DisplayNumber) => DisplayNumber {
+    return ({text, value}: DisplayNumber) => ({text: `${prefix} ${text}`, value});
   }
 
 }
