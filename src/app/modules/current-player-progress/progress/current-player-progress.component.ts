c482ada326d50409f08fb5b14c48b6e9c833dd3c
JustCode7 <harrypottz85@gmail.com>
2026-06-13 17:31:16 +0200

    feat: refactor playeroverview

--- src/app/modules/current-player-progress/progress/current-player-progress.component.ts
+++ src/app/modules/current-player-progress/progress/current-player-progress.component.ts
@@ -9,6 +9,7 @@ import {CommonModule} from "@angular/common";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MiniPlayerOverview} from "./mini-player-overview/mini-player-overview";
 
 @Component({
   selector: 'app-current-player-progress',
@@ -20,6 +21,7 @@ import {MatButtonModule} from "@angular/material/button";
     MatCardModule,
     MatIconModule,
     MatButtonModule,
+    MiniPlayerOverview,
   ],
 })
 export class CurrentPlayerProgressComponent {
