52adb68e84fbe25f82bdc26d7c421ff63785dac5
JustCode7 <harrypottz85@gmail.com>
2026-09-24 16:13:27 +0200

    feat: fullscreen bug

--- src/app/services/current-player.service.ts
+++ src/app/services/current-player.service.ts
@@ -503,8 +503,6 @@ export class CurrentPlayerService {
     if (data.player.history.length > 0) {
       this.dialog.open(HistoryDialog, {
         data,
-        maxWidth: '95vw',
-        maxHeight: '95vh',
         panelClass: 'full-screen-dialog-mobile'
       });
     }
